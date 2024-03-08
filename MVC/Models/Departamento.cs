7d795f6d00357f9ddc8f0f44616d277aa5a03494
Felipe Lopes Zem <felipelopeszem@hotmail.com>
2024-03-08 18:43:14 -0300

    models iniciais

---
+++ MVC/Models/Departamento.cs
@@ -0,0 +1,9 @@
+﻿namespace MVC.Models
+{
+    public class Departamento
+    {
+        public int Id { get; set; }
+        public string Codigo { get; set; }
+        public string Descricao { get; set; }
+    }
+}
---
+++ MVC/Models/Produto.cs
@@ -0,0 +1,39 @@
+﻿namespace MVC.Models
+{
+    public class Produto
+    {
+        /// <summary>
+        /// ID - Identificador do Produto - UUID
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Código - Código apresentável ao usuário - Texto
+        /// </summary>
+        public string Codigo { get; set; }
+
+
+        /// <summary>
+        /// Descrição - Descrição do Produto - Texto
+        /// </summary>
+        public string Descricao { get; set; }
+
+        /// <summary>
+        /// Departamento - Lista de departamentos - Caixa de Seleção (Será consumido via GET da api criada)
+        /// </summary>
+        public Departamento Departamento { get; set; }
+
+        /// <summary>
+        /// Preço - Preço do Produto - Decimal
+        /// </summary>
+        public decimal Preco { get; set; }
+
+        /// <summary>
+        /// Status - Ativo / Inativo - True/False - Booleano
+        /// </summary>
+        public bool Status { get; set; }
+
+        //public dynamic Editar { get; set; }
+        //public dynamic Excluir { get; set; }
+    }
+}
